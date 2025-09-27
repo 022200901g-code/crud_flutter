9d9fdc806c08dc775c9f826b881f9541890214df
edgar hugo <022200901g@uandina.edu.pe>
2025-09-27 10:00:24 -0500

    actividad 2

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 if (_EditTituloLibro.text.isNotEmpty) {
                   _agregarNuevoLibro(_EditTituloLibro.text.toString());
-                  //_EditTituloLibro.clear(); 
+                  EditTituloLibro.clear();
                   Navigator.of(context).pop();
                 }
               },
